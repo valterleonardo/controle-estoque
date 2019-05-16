e939b5e337bbd89c47218e03f1f3a293ae3ecaec
Valter Bortoletto <valter.bortoletto@redetendencia.com.br>
2019-05-15 20:34:39 -0400

    initial

---
+++ src/main/java/estoque/controle/ms/MsControleEstoqueApplication.java
@@ -0,0 +1,13 @@
+package estoque.controle.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsControleEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsControleEstoqueApplication.class, args);
+	}
+
+}
